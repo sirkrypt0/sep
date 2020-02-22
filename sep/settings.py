f74507ec6cbaf3a73c8636026fdf16bfe1068688
sirkrypt0 <tobiaskantusch@online.de>
2020-02-23 00:24:07 +0100

    Added Cross Origin Resource Sharing (CORS) for localhost
    Signed-off-by: sirkrypt0 <tobiaskantusch@online.de>

--- sep/settings.py
+++ sep/settings.py
@@ -39,11 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'semesterplaner.apps.SemesterplanerConfig',
+
     'rest_framework',
+    'corsheaders',
+
+    'semesterplaner.apps.SemesterplanerConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -80,6 +85,11 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
+
+# Cors settings
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
+]
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
