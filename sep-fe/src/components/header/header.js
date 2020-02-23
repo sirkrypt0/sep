e1aadae979157d96134c462ccca24f425da0224e
linaScience <maidofmedicine@gmail.com>
2020-02-23 12:34:44 +0100

    restored Header.js

--- sep-fe/src/components/header/header.js
+++ sep-fe/src/components/header/header.js
@@ -1,4 +1,19 @@
-import react from 'react'
+import React, {Component} from 'react';
+import {Nav, Navbar} from 'react-bootstrap';
+import './Header.css';
 
-
+class Header extends Component {
+    render() {
+        return (
+            <Navbar role="navigation">
+                <Navbar.Brand href="/4∞">Semesterplaner</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/myplans">Meine Pläne</Nav.Link>
+                        <Nav.Link href="/planner">Planer</Nav.Link>
+                    </Nav>
+            </Navbar>
+        )
+    }
+}
 export default Header;
