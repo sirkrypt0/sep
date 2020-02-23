cb9cc6d6c4dfa75cecbced2bdd13a0fec5113c00
sirkrypt0 <tobiaskantusch@online.de>
2020-02-23 01:09:49 +0100

    Moved hall to TimeSlot model Improved admin page for Lecture and TimeSlots Added timeslots to serialized Lecture
    Signed-off-by: sirkrypt0 <tobiaskantusch@online.de>

--- semesterplaner/admin.py
+++ semesterplaner/admin.py
@@ -5,5 +5,28 @@ from .models import *
 # Register your models here.
 
 
-admin.site.register(Lecture)
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = ('get_name', 'type', 'hall', 'weekday', 'time')
+
+    def get_name(self, obj):
+        return obj.lecture.title
+    get_name.admin_order_field = 'lecture'
+    get_name.short_description = 'Lecture Title'
+
+
+class TimeSlotInline(admin.StackedInline):
+    model = TimeSlot
+    extra = 0
+
+
+class LectureAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Lecture',     {'fields': ['title', 'lp', 'semester', 'lecturer']}),
+    ]
+    inlines = [TimeSlotInline]
+    list_display = ('title', 'lp', 'semester')
+
+
+admin.site.register(Lecture, LectureAdmin)
 admin.site.register(Lecturer)
+admin.site.register(TimeSlot, TimeSlotAdmin)
