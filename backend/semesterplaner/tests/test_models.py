e5506a0f82c620f4c01245d47c871bffce10182a
sirkrypt0 <tobiaskantusch@online.de>
2020-02-23 19:36:45 +0100

    Fixes
    Signed-off-by: sirkrypt0 <tobiaskantusch@online.de>

--- backend/semesterplaner/tests/test_models.py
+++ backend/semesterplaner/tests/test_models.py
@@ -1,5 +1,4 @@
 from datetime import time
-
 from django.test import TestCase
 
 # Create your tests here.
@@ -15,7 +14,7 @@ class LecturerModelTest(TestCase):
     def test_name_content(self):
         lecturer = Lecturer.objects.get(pk=1)
         expected_name = f'{lecturer.name}'
-        self.assertEquals(expected_name, 'Tester')
+        self.assertEqual(expected_name, 'Tester')
 
 
 class LectureModelTest(TestCase):
@@ -32,27 +31,27 @@ class LectureModelTest(TestCase):
     def test_title_content(self):
         lecture = Lecture.objects.get(pk=1)
         expected_title = f'{lecture.title}'
-        self.assertEquals(expected_title, 'Test_lecture')
+        self.assertEqual(expected_title, 'Test_lecture')
 
     def test_lp_content(self):
         lecture = Lecture.objects.get(pk=1)
         expected_lp = lecture.lp
-        self.assertEquals(expected_lp, 6)
+        self.assertEqual(expected_lp, 6)
 
     def test_semester_content(self):
         lecture = Lecture.objects.get(pk=1)
         expected_semester = lecture.semester
-        self.assertEquals(expected_semester, 2)
+        self.assertEqual(expected_semester, 2)
 
     def test_lecturer_content(self):
         lecture = Lecture.objects.get(pk=1)
         expected_lecturer = lecture.lecturer.name
-        self.assertEquals(expected_lecturer, 'Tester')
+        self.assertEqual(expected_lecturer, 'Tester')
 
     def test_description_content(self):
         lecture = Lecture.objects.get(pk=1)
         expected_description = lecture.description
-        self.assertEquals(expected_description, 'Test description')
+        self.assertEqual(expected_description, 'Test description')
 
 
 class TimeSlotModelTest(TestCase):
@@ -74,24 +73,24 @@ class TimeSlotModelTest(TestCase):
     def test_weekday_content(self):
         timeslot = TimeSlot.objects.get(pk=1)
         expected_weekday = timeslot.weekday
-        self.assertEquals(expected_weekday, 0)
+        self.assertEqual(expected_weekday, 0)
 
     def test_time_content(self):
         timeslot = TimeSlot.objects.get(pk=1)
         expected_time = timeslot.time
-        self.assertEquals(expected_time, time(9, 00))
+        self.assertEqual(expected_time, time(9, 00))
 
     def test_lecture_content(self):
         timeslot = TimeSlot.objects.get(pk=1)
         expected_lecture = timeslot.lecture.title
-        self.assertEquals(expected_lecture, 'Test_lecture')
+        self.assertEqual(expected_lecture, 'Test_lecture')
 
     def test_hall_content(self):
         timeslot = TimeSlot.objects.get(pk=1)
         expected_hall = timeslot.hall
-        self.assertEquals(expected_hall, 'HS 2')
+        self.assertEqual(expected_hall, 'HS 2')
 
     def test_type_content(self):
         timeslot = TimeSlot.objects.get(pk=1)
         expected_type = timeslot.type
-        self.assertEquals(expected_type, 0)
+        self.assertEqual(expected_type, 0)
