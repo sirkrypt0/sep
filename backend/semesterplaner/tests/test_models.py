8a7ad01278b1cab7faac1451820a07a12e8a6f2f
sirkrypt0 <tobiaskantusch@online.de>
2020-02-24 00:19:05 +0100

    Added travis ci for backend (#7)
    * Create .travis.yml
    
    * Fixed test_models
    
    Signed-off-by: sirkrypt0 <tobiaskantusch@online.de>

--- backend/semesterplaner/tests/test_models.py
+++ backend/semesterplaner/tests/test_models.py
@@ -12,7 +12,7 @@ class LecturerModelTest(TestCase):
         Lecturer.objects.create(name='Tester')
 
     def test_name_content(self):
-        lecturer = Lecturer.objects.get(pk=1)
+        lecturer = Lecturer.objects.first()
         expected_name = f'{lecturer.name}'
         self.assertEqual(expected_name, 'Tester')
 
@@ -25,31 +25,31 @@ class LectureModelTest(TestCase):
         Lecture.objects.create(title='Test_lecture',
                                lp=6,
                                semester=2,
-                               lecturer_id=1,
+                               lecturer_id=Lecturer.objects.first().pk,
                                description='Test description')
 
     def test_title_content(self):
-        lecture = Lecture.objects.get(pk=1)
+        lecture = Lecture.objects.first()
         expected_title = f'{lecture.title}'
         self.assertEqual(expected_title, 'Test_lecture')
 
     def test_lp_content(self):
-        lecture = Lecture.objects.get(pk=1)
+        lecture = Lecture.objects.first()
         expected_lp = lecture.lp
         self.assertEqual(expected_lp, 6)
 
     def test_semester_content(self):
-        lecture = Lecture.objects.get(pk=1)
+        lecture = Lecture.objects.first()
         expected_semester = lecture.semester
         self.assertEqual(expected_semester, 2)
 
     def test_lecturer_content(self):
-        lecture = Lecture.objects.get(pk=1)
+        lecture = Lecture.objects.first()
         expected_lecturer = lecture.lecturer.name
         self.assertEqual(expected_lecturer, 'Tester')
 
     def test_description_content(self):
-        lecture = Lecture.objects.get(pk=1)
+        lecture = Lecture.objects.first()
         expected_description = lecture.description
         self.assertEqual(expected_description, 'Test description')
 
@@ -62,35 +62,35 @@ class TimeSlotModelTest(TestCase):
         Lecture.objects.create(title='Test_lecture',
                                lp=6,
                                semester=2,
-                               lecturer_id=1,
+                               lecturer_id=Lecturer.objects.first().pk,
                                description='Test description')
         TimeSlot.objects.create(weekday=0,
                                 time=time(9, 00),
-                                lecture_id=1,
+                                lecture_id=Lecture.objects.first().pk,
                                 hall='HS 2',
                                 type=0)
 
     def test_weekday_content(self):
-        timeslot = TimeSlot.objects.get(pk=1)
+        timeslot = TimeSlot.objects.first()
         expected_weekday = timeslot.weekday
         self.assertEqual(expected_weekday, 0)
 
     def test_time_content(self):
-        timeslot = TimeSlot.objects.get(pk=1)
+        timeslot = TimeSlot.objects.first()
         expected_time = timeslot.time
         self.assertEqual(expected_time, time(9, 00))
 
     def test_lecture_content(self):
-        timeslot = TimeSlot.objects.get(pk=1)
+        timeslot = TimeSlot.objects.first()
         expected_lecture = timeslot.lecture.title
         self.assertEqual(expected_lecture, 'Test_lecture')
 
     def test_hall_content(self):
-        timeslot = TimeSlot.objects.get(pk=1)
+        timeslot = TimeSlot.objects.first()
         expected_hall = timeslot.hall
         self.assertEqual(expected_hall, 'HS 2')
 
     def test_type_content(self):
-        timeslot = TimeSlot.objects.get(pk=1)
+        timeslot = TimeSlot.objects.first()
         expected_type = timeslot.type
         self.assertEqual(expected_type, 0)
